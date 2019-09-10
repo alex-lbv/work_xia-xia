64413b225f075dc944b9f23a285abfe1f6f6ae77
godjo <aal940722@gmail.com>
2019-09-10 16:28:29 +0300

    feature: adds articles for index.html

--- src/js/components/swiper-slider.js
+++ src/js/components/swiper-slider.js
@@ -2,3 +2,20 @@ var swiper = new Swiper('.swiper-container', {
   slidesPerView: 4,
   spaceBetween: 10,
 });
+
+var swiperArticles = new Swiper('.swiper-articles', {
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+  spaceBetween: 10,
+  slidesPerView: 4,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    1200: {
+      slidesPerView: 2,
+    }
+  },
+});
