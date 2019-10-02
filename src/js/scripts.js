4fe2758d7c1d990195c23e6023b462cf6aeac1f9
godjo <aal940722@gmail.com>
2019-10-02 18:41:24 +0300

    feature: repait header and product card

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,19 @@
 //= nice-select.js
 
 document.addEventListener("DOMContentLoaded", function () {
-//= components/swiper-slider.js
-//= components/select.js
+  //= components/swiper-slider.js
+  //= components/select.js
 });
+
+$("body").on('focus', '#searchInput', function () {
+  $('header').addClass('search-active');
+});
+
+$("body").on('blur', '#searchInput', function () {
+  $('header').removeClass('search-active');
+});
+
+
+/* $("#searchInput").on('keyup', function () {
+  $('.search-results').addClass('active');
+}); */
